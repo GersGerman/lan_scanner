8f3517135b1d84d277bbe4297b62b75f8d8b441e
GersGerman <glyskof@gmail.com>
2025-02-04 12:37:16 +0300

    mnogo workal

--- windows/web/mainapp/urls.py
+++ windows/web/mainapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index, telegrambot, listip
+from .views import index, telegrambot, listip, get_ips
 
 urlpatterns = [
     path("", name="index-page", view=index),
     path("telegram", name="telegrambot", view=telegrambot),
     path("ip", name="listip", view=listip),
+
+    path("getdata/ip", name='ajax-ip', view=get_ips)
+
 ]
--- windows/web/mainapp/views.py
+++ windows/web/mainapp/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def index(request):
     return render(request, "index.html")
+
+
+def get_ips(request):
+
+    return JsonResponse({"trused": {
+                            "127.0.0.1": 'Galaxy A51',
+                            "127.0.0.2": 'Galaxy A52',},
+                        "new":{
+                            "127.0.0.3": 'Galaxy A53',
+                            "127.0.0.4": 'Galaxy A54',
+                            "127.0.0.5": 'Galaxy A55',
+            }
+        })
+
 
 def telegrambot(request):
     return render(request, 'telegram.html')
