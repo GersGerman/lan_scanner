d485f864686ed723a6a859c64afefea105b8dfc8
g3rs_0ne <163648802+GersGerman@users.noreply.github.com>
2025-02-08 13:43:15 +0300

    workal

--- windows/main.py
+++ windows/main.py
@@ -1,3 +1,5 @@
+import sys
+from time import sleep
 from colorama import init, Fore
 init(autoreset=True)
 def main():
@@ -6,12 +8,21 @@ def main():
 
     print("Запуск серверов: Сканер сети и web сервер....")
 
-    # os.system(f"{os.getcwd()}\windows\scripts\server.exe")
     print(Fore.MAGENTA+os.getcwd()+"/\scripts/\server.exe")
     subprocess.Popen(os.getcwd()+"\scripts\server.exe",)
-    # os.system(f"python {os.getcwd()}\windows\web\manage.py runserver")
     subprocess.Popen(f"python {os.getcwd()}/\web/\manage.py runserver 0.0.0.0:8089")
+    
+    while True:
+        try:
+            sleep(0.1)
+        
+        except KeyboardInterrupt:
+            subprocess.Popen("taskkill /f /im server.exe")
+            break
+        else:
+            pass
 
+    sys.exit()   
 
 if __name__ == "__main__":
     main()
