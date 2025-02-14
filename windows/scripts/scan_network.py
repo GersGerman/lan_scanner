5a44d67a0f19f306aeeeb55ff8876b70495d734b
g3rs_0ne <163648802+GersGerman@users.noreply.github.com>
2025-02-14 20:44:57 +0300

    надо во вьюхах доделать чтобы он правильно отправлял запросы

--- windows/scripts/scan_network.py
+++ windows/scripts/scan_network.py
@@ -1,13 +1,13 @@
-# import socket as sk
+import socket as sk
 
-# sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 
-# sock.connect(("127.0.0.1", 6535))
+sock.connect(("127.0.0.1", 6535))
 
-# print(sock.recv(1024))
+print(sock.recv(1024))
 
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-print(s.getsockname()[0])
-s.close()
+# import socket
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.connect(("", 6365))
+# print(s.getsockname()[0])
+# s.close()
